618f754e8cdcdbf0a1ea79fd7d50513b9b46e98a
Kelly-Walsh Michael <pfai001@michael.home>
2026-06-07 12:05:01 +0200

    Reformatted for clarity

--- flight_data_manipulation.py
+++ flight_data_manipulation.py
@@ -3,11 +3,11 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 import matplotlib.dates as mdates
 
 # Working with the flights dataset
-df = sns.load_dataset('flights')
+df = sns.load_dataset("flights")
 
 # Exploring the data
 # df.head()
@@ -19,29 +19,29 @@ df = sns.load_dataset('flights')
 # max(df.passengers)
 
 # Manipulating the columns
-df['month_year'] = df['month'].astype(str) + '-' + df['year'].astype(str)
-df['date'] = pd.to_datetime(df['month_year'], format='%b-%Y')
-df.drop(columns = ['month_year'], inplace=True)
+df["month_year"] = df["month"].astype(str) + "-" + df["year"].astype(str)
+df["date"] = pd.to_datetime(df["month_year"], format="%b-%Y")
+df.drop(columns=["month_year"], inplace=True)
 # df.loc[df['date'] < '1950-08']
 
 # Average number of passengers per month over all years
-df.groupby('month').agg({'passengers':'mean'}).sort_values('passengers')
+df.groupby("month").agg({"passengers": "mean"}).sort_values("passengers")
 
 # Average number of passengers per year over all months
-df.groupby('year').agg({'passengers':'mean'}).sort_values('passengers')
+df.groupby("year").agg({"passengers": "mean"}).sort_values("passengers")
 
 # Plot passengers by date -----------------------------------
 
 # Convert dates to numbers to create line of best fit
-datesAsNums = mdates.date2num(df['date'])
-slope, intercept = np.polyfit(datesAsNums, df['passengers'], 1)
+datesAsNums = mdates.date2num(df["date"])
+slope, intercept = np.polyfit(datesAsNums, df["passengers"], 1)
 line = slope * datesAsNums + intercept
 
 # Create plot
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.plot(df['date'], slope * datesAsNums + intercept, color='red', label='Best Fit Line')
-ax.scatter(df['date'], df['passengers'])
-plt.xlabel('Date of flight.')
-plt.ylabel('Number of passengers.')
-plt.title('Number of passengers flying on a given date.')
+ax.plot(df["date"], slope * datesAsNums + intercept, color="red", label="Best Fit Line")
+ax.scatter(df["date"], df["passengers"])
+plt.xlabel("Date of flight.")
+plt.ylabel("Number of passengers.")
+plt.title("Number of passengers flying on a given date.")
 plt.show()
