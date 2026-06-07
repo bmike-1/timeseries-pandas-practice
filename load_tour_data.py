3aa6f211ac587ad71e9ad3493cdd879ec43838ff
Kelly-Walsh Michael <pfai001@michael.home>
2026-06-07 12:05:44 +0200

    Load practice wiki data from kaggle for csv manipulation

--- load_tour_data.py
+++ load_tour_data.py
@@ -10,16 +10,15 @@ file_path = "my_file (1).csv"
 
 # Load the latest version
 df = kagglehub.load_dataset(
-  KaggleDatasetAdapter.PANDAS,
-  "amruthayenikonda/dirty-dataset-to-practice-data-cleaning",
-  file_path,
-  # Provide any additional arguments like 
-  # sql_query or pandas_kwargs. See the 
-  # documenation for more information:
-  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+    KaggleDatasetAdapter.PANDAS,
+    "amruthayenikonda/dirty-dataset-to-practice-data-cleaning",
+    file_path,
+    # Provide any additional arguments like
+    # sql_query or pandas_kwargs. See the
+    # documenation for more information:
+    # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
 )
 
 # df.head()
 
-df.to_csv("input_data/tour_data.csv", index = False)
-
+df.to_csv("input_data/tour_data.csv", index=False)
